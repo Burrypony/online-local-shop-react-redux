943bcacc1ee70125983d30c0454a63c09cd2c8e0
Burrypony <kiril.chernysh@gmail.com>
2019-05-20 20:21:27 +0300

    first commit

---
+++ src/pages/cartpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Cart from '../features/cart'
+
+export default function Cartpage(props){
+    return <div>
+        <h2>
+            My cards
+        </h2>
+        <Cart />
+    </div>
+}
